45cfd3967c20aa976e8dba9f9076aea238e09b9d
Kanako31710 <dxkanako.f.ie@gmail.com>
2025-07-06 22:34:23 +0900

    Add files via upload

---
+++ kadai_007/clac.js
@@ -0,0 +1,11 @@
+let number =15;
+
+if (number % 3 === 0){
+    console.log('３の倍数です');
+}
+if(number % 5 === 0){
+    console.log('５の倍数です');
+}
+if(number % 5 ===0 && number % 3 ===0){
+    console.log('３と５の倍数です')
+}
