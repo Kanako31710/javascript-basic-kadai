0245bd14fe0d4d8e981975dc6c087a4707a52af0
Kanako31710 <dxkanako.f.ie@gmail.com>
2025-07-14 17:20:06 +0900

    Delete kadai_020 directory

--- kadai_020/event.js
+++
@@ -1,9 +0,0 @@
-// output-btnというidを持つHTML要素を取得し、定数を代入する。
-const btn = document.getElementById('btn')
-const parentList = document.getElementById('text');
-// 　HTML要素がクリックされた特にイベントの処理を実行する
-btn.addEventListener( 'click', ()=>{
-    const childtext = document.createElement('h2');
-    text.textContent = 'ボタンをクリックしました';
-   
-});
